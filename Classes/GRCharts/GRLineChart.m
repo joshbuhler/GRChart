ba63e8ecb0b73bf9501d46566ddeef73edd15d3e
Josh Buhler <josh@joshbuhler.com>
2010-03-09 20:03:21 -0700

    Plugged a few memory leaks

--- Classes/GRCharts/GRLineChart.m
+++ Classes/GRCharts/GRLineChart.m
@@ -361,6 +361,9 @@
 	range.min = [[minValues valueForKeyPath:@"@min.floatValue"] floatValue];
 	range.max = [[maxValues valueForKeyPath:@"@max.floatValue"] floatValue];
 	
+	[minValues release];
+	[maxValues release];
+	
 	return range;
 }
 
